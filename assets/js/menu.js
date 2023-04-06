0d929c06b791e01b00e8ac7c923697edb7eddef8
Angelo <92029180+Anbfer@users.noreply.github.com>
2023-04-06 17:20:28 -0300

    Atualização severa em responsividade do layout, menu e formulário

---
+++ assets/js/menu.js
@@ -0,0 +1,11 @@
+// document.querySelector("#hamburguer").addEventListener("click", mostrarMenu)
+
+// function mostrarMenu() {
+//     document.querySelector("nav").classList.toggle("menu-show")
+// }
+
+document.querySelector("#hamburguer").addEventListener("click",() =>
+    document.querySelector("nav").classList.toggle("menu-show")
+)
+
+// paradiguima funcional funcao anonima e arrow function
--- js/loader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    document.querySelector(".loader").classList.add("hiden")
-    document.querySelector("main").classList.remove("hiden")
-
-
-})
